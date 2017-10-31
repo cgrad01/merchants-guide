ed6515c0fa4c682257075397771bce4a1aebe307
Corey Grad <cgrad01@gmail.com>
2017-10-31 09:12:26 -0400

    answer first question

--- src/UnitConverter.java
+++ src/UnitConverter.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+
 public class UnitConverter extends QuestionReader {
     private String question;
+//    private ArrayList<CurrencyUnit> quantities;
+    private String quantity = "";
+    private Integer value;
 
     @Override
     boolean isQuestion(String line) {
@@ -14,11 +19,26 @@ public class UnitConverter extends QuestionReader {
 
     @Override
     void setQuantity() {
+        String[] words = question.split(" ");
+        for (String word : words) {
+            if (Runner.UNIT_VALUES.containsKey(word)){
+                quantity += word + " ";
+            }
+        }
+        setValue();
+    }
 
+    void setValue() {
+        String[] units = quantity.split(" ");
+        String numeral = "";
+        for (String unit : units) {
+            numeral += Runner.UNIT_VALUES.get(unit);
+            value = new Numeral(numeral).getValue();
+        }
     }
 
     @Override
     void displayAnswer() {
-        Runner.QUESTION_ANSWERS.add("Answer 1");
+        Runner.QUESTION_ANSWERS.add(quantity + "is " + value);
     }
 }
